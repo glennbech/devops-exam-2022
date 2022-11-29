8b027c8dc14610644da87578c94245d4117f2fa4
Thomas Nemeth <thonem10@gmail.com>
2022-11-29 21:30:23 +0100

    values

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -1,6 +1,5 @@
 resource "aws_cloudwatch_dashboard" "main" {
   dashboard_name = var.candidate_id
-## Jim; seriously! we can use any word here.. How cool is that?
   dashboard_body = <<DEATHSTAR
 {
   "widgets": [
@@ -14,13 +13,13 @@ resource "aws_cloudwatch_dashboard" "main" {
         "metrics": [
           [
             "${var.candidate_id}",
-            "account_count.value"
+            "carts_count.value"
           ]
         ],
         "period": 300,
         "stat": "Maximum",
         "region": "eu-west-1",
-        "title": "Total number of accounts"
+        "title": "Total number of carts"
       }
     }
   ]
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,9 @@
 variable "candidate_id" {
   type = string
+  default = "1022"
 }
 
 variable "candidate_email"  {
   type = string
+  default = "thonem10@gmail.com"
 }
