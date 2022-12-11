70ae39bbe34ad0f13d830647acec13e6c145199d
thonems <thonem10@gmail.com>
2022-12-11 10:28:50 +0000

    alarm

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -1,3 +1,20 @@
+resource "aws_cloudwatch_metric_alarm" "zerosum" {
+  alarm_name                = "shpping-cart-alarm-greater-than-5!"
+  namespace                 = "1022"
+  metric_name               = "carts_count.value"
+
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "5"
+  evaluation_periods        = "3"
+  period                    = "15"
+
+  statistic                 = "Maximum"
+
+  alarm_description         = "goes off when cart over 5 in 15 min "
+  insufficient_data_actions = []
+  alarm_actions       = [aws_sns_topic.user_updates.arn]
+}
+
 resource "aws_sns_topic" "alarms" {
   name = "alarm-topic-${var.candidate_id}"
 }
